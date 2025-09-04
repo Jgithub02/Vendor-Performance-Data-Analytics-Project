7397bb8aa4629fd315c8dc9d3dc9e53750f14914
Jitendra Kumar <164842901+Jgithub02@users.noreply.github.com>
2025-09-04 08:20:12 +0530

    Add files via upload

---
+++ Vendor Performance Data Analytics.py
@@ -0,0 +1,7 @@
+import pandas as pd 
+import os 
+from sqlalchemy import create_engine
+engine = create_engine('sqlite:///:inventry.db')
+for file in os.listdir(r'C:\Users\Jitendra\OneDrive\Desktop\Venders'):
+    print(file)
+   
